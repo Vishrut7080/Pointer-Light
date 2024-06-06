dedfbd7a3c353a0fd09869de80bcc7b917de5983
Vishrut <165580939+Vishrut7080@users.noreply.github.com>
2024-06-06 20:27:06 +0530

    Setup tracking of cursor

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+const seeThrough = document.getElementById("see-through");
+
+document.addEventListener('mousemove', (e) => {
+    const posX = e.x
+    const posY = e.y
+    console.log(posX, posY)
+    seeThrough.style.left = `${posX}px`;
+    seeThrough.style.top = `${posY}px`;
+})
